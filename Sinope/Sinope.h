1f1e912fdcc1848a986d302b15d5bf4b113f4eb1
Rachel Brindle <you@subluminal.net>
2016-07-07 21:44:33 -0700

    Initial Commit

---
+++ Sinope/Sinope.h
@@ -0,0 +1,19 @@
+//
+//  Sinope.h
+//  Sinope
+//
+//  Created by Rachel Brindle on 7/7/16.
+//  Copyright © 2016 Rachel Brindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sinope.
+FOUNDATION_EXPORT double SinopeVersionNumber;
+
+//! Project version string for Sinope.
+FOUNDATION_EXPORT const unsigned char SinopeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sinope/PublicHeader.h>
+
+
