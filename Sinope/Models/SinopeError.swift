d3eae536cba2cdf51c9414147e73523283889599
Rachel Brindle <you@subluminal.net>
2016-10-17 07:31:13 -0700

    Add error messaging

--- Sinope/Models/SinopeError.swift
+++ Sinope/Models/SinopeError.swift
@@ -1,6 +1,19 @@
-public enum SinopeError: Error, Equatable {
+public enum SinopeError: Error {
     case network
     case json
     case notLoggedIn
     case unknown
+
+    var localizedDescription: String {
+        switch self {
+        case .network:
+            return NSLocalizedString("Error_Network", comment: "")
+        case .json:
+            return NSLocalizedString("Error_JSON", comment: "")
+        case .notLoggedIn:
+            return NSLocalizedString("Error_NotLoggedIn", comment: "")
+        case .unknown:
+            return NSLocalizedString("Error_Unknown", comment: "")
+        }
+    }
 }
