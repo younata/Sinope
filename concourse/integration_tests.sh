fc508b7de0074fc0cdca1fa95031fa0495f5ff32
Rachel Brindle <you@subluminal.net>
2016-11-16 09:45:26 -0800

    change how integration tests run on concourse

--- concourse/integration_tests.sh
+++ concourse/integration_tests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cd sinope_github
+pushd pasiphae_github
+bundle install --gemfile=./Gemfile && bundle exec rake db:migrate && PASIPHAE_APPLICATION_TOKEN=\'test\' bundle exec rails s &
+sleep 10 
+popd
+pushd sinope_github
 bundle
-bundle exec rake test:integration:setup_and_run
+bundle exec rake test:integration:run
