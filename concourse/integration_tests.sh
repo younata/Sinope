e719f6e2728278a4c42e3e3200427b2385b4ee58
Rachel Brindle <you@subluminal.net>
2016-11-16 10:29:25 -0800

    kill pasiphae after integration tests run

--- concourse/integration_tests.sh
+++ concourse/integration_tests.sh
@@ -4,9 +4,11 @@ pushd pasiphae_github
 bundle install --gemfile=./Gemfile
 bundle exec rake db:migrate
 PASIPHAE_APPLICATION_TOKEN='test' bundle exec rails s &
+PASIPHAE_PID=$!
 sleep 10 
 popd
 pushd sinope_github
 bundle install --gemfile=./Gemfile
 bundle exec carthage_cache -b $AWS_CACHE_BUCKET install || (carthage bootstrap --platform ios,mac --no-use-binaries; bundle exec carthage_cache -b $AWS_CACHE_BUCKET publish)
 bundle exec rake test:integration:run
+kill -9 $PASIPHAE_PID
